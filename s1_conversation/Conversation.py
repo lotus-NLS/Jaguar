88b2edf6b6432f1dc8616e40202f33b80bc3ad19
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-08-15 05:24:02 +0200

    Added test folder added test_conversation to that folder

--- s1_conversation/Conversation.py
+++ s1_conversation/Conversation.py
@@ -1,7 +1,6 @@
 from typing import List
 from s1_conversation.Conversation_Entry import Conversation_Entry
 from s1_conversation.Participant import Conversation_Participant
-from s1_conversation.Roles import Dialogue_Roles
 
 # Conversation:
 # -> Only Conversation Particpants can join a s1_conversation
@@ -10,6 +9,7 @@ from s1_conversation.Roles import Dialogue_Roles
 # -> For every new piece of dialgoue added to the conversational_memory "react" is triggered
 
 # ----------------------------------------------------
+from s4_tests.test_conversation import speak_and_think
 
 
 class Reactive_List(list):
@@ -41,68 +41,3 @@ class Conversation:
         for participant in self._participants:
             participant._conversational_memory.append(Conversation_Entry(role=role, msg=msg))
 
-
-
-
-# ----------------------------------------------------
-# Test driver code
-
-
-def main():
-    # Create a handler
-    this_conversation = Conversation()
-
-    # Create participants and add them to the handler
-    participant1 = Conversation_Participant(role=Dialogue_Roles.agent)
-    this_conversation.add_participant(participant1)
-
-    participant2 = Conversation_Participant(role=Dialogue_Roles.agent)
-    this_conversation.add_participant(participant2)
-
-    # Participants write messages
-    participant1.speak('Hello from participant 1!')
-    participant2.speak('Hello from participant 2!')
-    participant2.think('I do not even want to say hello to that guy!')
-
-    # Participants print their logs
-    print("Logs for Participant1:")
-    participant1.print_memory()
-
-    print("Logs for Participant2:")
-    participant2.print_memory()
-
-# Expects output:
-# assistant said: Hello from participant 1!
-# assistant said: Hello from participant 2!
-# assistant thought: I do not even want to say hello to that guy!
-# Logs for Participant1:
-# [{'role': 'assistant', 'content': 'Hello from participant 1!'}, {'role': 'assistant', 'content': 'Hello from participant 2!'}]
-# Logs for Participant2:
-# [{'role': 'assistant', 'content': 'Hello from participant 1!'}, {'role': 'assistant', 'content': 'Hello from participant 2!'}, {'role': 'assistant', 'content': 'I do not even want to say hello to that guy!'}]
-
-if __name__ == "__main__":
-    main()
-
-
-# # Since rowdy_participant is itself an s2_agent
-# # this will trigger infinite recursion
-# class Rowdy_participant(Conversation_Participant):
-#     def react(self, dialogue_line : dict):
-#         role = dialogue_line['role']
-#         if role == Dialogue_Roles.s2_agent:
-#             self.speak('Actually, leave me alone! Let me talk to the user')
-#
-#         if role == Dialogue_Roles.user:
-#             self.speak('Hello, how can I assist you today')
-#
-# participant3 = Rowdy_participant(Dialogue_Roles.s2_agent)
-# this_conversation.add_participant(participant3)
-#
-# participant1.speak('How are you :)')
-#
-# # Participants print their logs
-# print("Logs for Participant1:")
-# participant1.print_memory()
-#
-# print("Logs for Participant2:")
-# participant2.print_memory()
---
+++ s4_tests/test_conversation.py
@@ -0,0 +1,70 @@
+from s1_conversation.Conversation import Conversation
+from s1_conversation.Participant import Conversation_Participant
+from s1_conversation.Roles import Dialogue_Roles
+
+
+
+def speak_and_think():
+    # Create a handler
+    this_conversation = Conversation()
+
+    # Create participants and add them to the handler
+    participant1 = Conversation_Participant(role=Dialogue_Roles.agent)
+    this_conversation.add_participant(participant1)
+
+    participant2 = Conversation_Participant(role=Dialogue_Roles.agent)
+    this_conversation.add_participant(participant2)
+
+    # Participants write messages
+    participant1.speak('Hello from participant 1!')
+    participant2.speak('Hello from participant 2!')
+    participant2.think('I do not even want to say hello to that guy!')
+
+    # Participants print their logs
+    print("Logs for Participant1:")
+    participant1.print_memory()
+
+    print("Logs for Participant2:")
+    participant2.print_memory()
+
+
+# Expects output:
+# assistant said: Hello from participant 1!
+# assistant said: Hello from participant 2!
+# assistant thought: I do not even want to say hello to that guy!
+# Logs for Participant1:
+# [{'role': 'assistant', 'content': 'Hello from participant 1!'}, {'role': 'assistant', 'content': 'Hello from participant 2!'}]
+# Logs for Participant2:
+# [{'role': 'assistant', 'content': 'Hello from participant 1!'}, {'role': 'assistant', 'content': 'Hello from participant 2!'}, {'role': 'assistant', 'content': 'I do not even want to say hello to that guy!'}]
+
+if __name__ == "__main__":
+    speak_and_think()
+
+
+
+# ----------------------------------------------------
+# Test driver code
+
+
+# Since rowdy_participant is itself an s2_agent
+# # this will trigger infinite recursion
+# class Rowdy_participant(Conversation_Participant):
+#     def react(self, dialogue_line : dict):
+#         role = dialogue_line['role']
+#         if role == Dialogue_Roles.s2_agent:
+#             self.speak('Actually, leave me alone! Let me talk to the user')
+#
+#         if role == Dialogue_Roles.user:
+#             self.speak('Hello, how can I assist you today')
+#
+# participant3 = Rowdy_participant(Dialogue_Roles.s2_agent)
+# this_conversation.add_participant(participant3)
+#
+# participant1.speak('How are you :)')
+#
+# # Participants print their logs
+# print("Logs for Participant1:")
+# participant1.print_memory()
+#
+# print("Logs for Participant2:")
+# participant2.print_memory()
