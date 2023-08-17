03b6d182dd77d3c9860dd7d99db4275326dfcf2e
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-08-17 23:57:07 +0200

    Added actions attribute to agenda entry

--- s1_protocol/AgendaEntry.py
+++ s1_protocol/AgendaEntry.py
@@ -2,6 +2,7 @@ import uuid
 
 # ----------------------------------------------------
 
+
 class AgendaEntry:
     @classmethod
     def make_root(cls, name, instruction):
@@ -10,26 +11,27 @@ class AgendaEntry:
     def __init__(self,name, instruction, is_root = False):
         self.name: str = f'++++ {name} ++++'
         self.desc: str = f'Instructions: {instruction}'
-        self.is_complete = False
+        self.is_complete : bool = False
 
         self._uuid = f'{uuid.uuid4()}-{uuid.uuid4()}'
         self._is_root = is_root
-        self.children_list: list[AgendaEntry] = []
         self.subelement_dictionary : dict[str, AgendaEntry] = {}
 
-    def edit_name(self, new_name):
+        self.actions = [self.edit_name, self.edit_description, self.mark_complete, self.add_subelement]
+
+
+    def edit_name(self, new_name : str):
         self.name = new_name
 
-    def edit_description(self, new_desc):
+    def edit_description(self, new_desc : str):
         self.desc = new_desc
 
     def mark_complete(self):
         self.is_complete = True
 
-    def _add_subelement(self, name : str, instruction: str):
+    def add_subelement(self, name : str, instruction: str):
         new_element = AgendaEntry(name=name, instruction=instruction)
         new_element._is_root = False
-        self.children_list.append(new_element)
         self.subelement_dictionary[new_element._uuid] = new_element
 
 
@@ -43,31 +45,28 @@ class AgendaEntry:
                    f'{space}Is done?: {completion_str}\n' \
                    f'{space}{self.desc}\n'
 
-        if self.children_list:
+        children_list = self.subelement_dictionary.values()
+        if children_list:
             obj_string += f'{space}Subtasks: \n'
-            for child_task in self.children_list:
+            for child_task in children_list:
                 obj_string += child_task.__str__(indent=indent + 1)
 
         return obj_string
 
 
 class Task(AgendaEntry):
-    def add_subtask(self, name, instruction):
-        return self._add_subelement(name,instruction)
-
+    pass
 
 class Objective(AgendaEntry):
-    def add_subobjective(self, name, instruction):
-        return self._add_subelement(name, instruction)
-
+    pass
 
 
 # Example usage:
 root_task = Task.make_root("Complete project", "Finish the software project by end of month.")
-root_task.add_subtask("Write code", "Implement the main features.")
-root_task.add_subtask("Test", "Make sure there are no bugs.")
+root_task.add_subelement("Write code", "Implement the main features.")
+root_task.add_subelement("Test", "Make sure there are no bugs.")
 print(root_task)
 
 root_objective = Objective.make_root("Increase user engagement", "Aim for 20% more daily active users.")
-root_objective.add_subobjective("Optimize UI", "Redesign the main landing page for better user experience.")
+root_objective.add_subelement("Optimize UI", "Redesign the main landing page for better user experience.")
 print(root_objective)
--- s1_protocol/Directive.py
+++ s1_protocol/Directive.py
@@ -5,4 +5,3 @@ class Directive:
     def __init__(self,task, objective):
         self.current_task : Task = task
         self.current_objective : Objective = objective
-
