57266b94340cb90f39e2b56f6ed4effb23f8d35d
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-08-18 03:10:14 +0200

    Removed finished todo

--- s4_UI/run_command_line.py
+++ s4_UI/run_command_line.py
@@ -12,17 +12,6 @@ test_conversation.add_participant(the_user)
 other_user = Conversation_Participant(role=Dialogue_Roles.user)
 test_conversation.add_participant(other_user)
 
-
-# TODO: The processing of the messages occuring immediately after the message is spoken leads
-# to the wrong ordering of messages for other s1_conversation participants
-# Because "react" of the bot triggers its own speak which is processed before before the
-# outer speak command of the user
-
-# I think that it could be solved by making reactions into seperate threads but that will still
-# involve a race condition.
-# Additionally, I'm not sure if I want the processing to go on while the s1_conversation can continue
-# This should be discussed.
-
 while True:
     the_user.speak(input(''))
 
