1f91922fdaa4d3ceb5dc1c6f571fb3e8db9f849b
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-08-26 11:24:13 +0200

    manual inquiry added

--- s4_run/run_command_line.py
+++ s4_run/run_command_line.py
@@ -3,7 +3,7 @@ from s3_tool_agent.BasicAgent import BasicAgent
 from s1_conversation.Conversation import ConversationParticipant,Channel, DialogueRole,enter_into_conversation
 
 # TODO: Collect common functionalities of run_command_line and run_GUI together
-# it would probably best to do this through a test module and elimitage s4_run altogether
+# it would probably best to do this through a dedicated RUN module that unifies command line and GUI
 # TODO: Should prompt the user to set the API key if it's not already set introduce set_api_key_if_needed()
 # i.e. check for the key where you expect it then prompt if you can't find it.
 # Don't have to check the functionality of the key there since that's done in the model and the
@@ -17,6 +17,10 @@ basic_channel = Channel()
 enter_into_conversation(channel=basic_channel,participant_list=[the_bot,the_user])
 
 # pdf_file_path = '/home/aiproj/Downloads/sample.pdf'
+
+# TODO: The agent can introduce itself. Basically it's its own instruction manual.
+# This also should be general to all run applications
+the_user.speak('[Manual inquiry for user]: Who are you and what can you do?')
 
 while True:
     the_user.speak(input(''))
