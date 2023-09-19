14675042e35f861b138755a141069f7f6e4f1e55
Daniel <daniel.hollarek@googlemail.com>
2023-09-19 17:11:57 +0200

    Reintroduced persitent terminal RUN

--- src/l1_lotus_tools/m0_toolbox/run.py
+++ src/l1_lotus_tools/m0_toolbox/run.py
@@ -3,7 +3,8 @@ import tempfile
 import subprocess
 import platform
 import threading
-import queue
+from typing import Optional
+from subprocess import Popen
 import time
 
 from src.l2_lotus_core import Tool,ToolArg
@@ -11,60 +12,80 @@ from src.l2_lotus_core import Tool,ToolArg
 # ---------------------------------------------------------
 
 
-
-
-
-class CommandResult:
-    def __init__(self, stdout='', stderr=''):
-        self.stdout = stdout
-        self.stderr = stderr
-
 class RUN(Tool):
     python_script_mode = 'py'
     cmd_mode = 'terminal'
+    os_in_use = platform.system()
 
     def __init__(self):
         super().__init__()
-
-        shell_cmd = 'cmd.exe' if platform.system() == 'Windows' else '/bin/sh'
-        try:
-            self.shell_session = subprocess.Popen(shell_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        except Exception as e:
-            print(f'[Error]: An exception occured while trying to start terminal session using {shell_cmd}: {e}')
-            self.shell_session = None
-
-        self.stdout_q = queue.Queue()
-        self.stderr_q = queue.Queue()
-
-        self.stdout_thread = threading.Thread(target=self.reader_thread, args=(self.shell_session.stdout, self.stdout_q))
-        self.stderr_thread = threading.Thread(target=self.reader_thread, args=(self.shell_session.stderr, self.stderr_q))
-
-        self.stdout_thread.daemon = True
-        self.stderr_thread.daemon = True
-
-        self.stdout_thread.start()
-        self.stderr_thread.start()
-
-
         self.description = 'The RUN tool allows you to either run a Python script or execute a command line command as input string.'
         self.mode_arg: ToolArg = self.create_arg(
             name='mode', dtype=str,
             available_options=[RUN.python_script_mode,RUN.cmd_mode],
             desc='')
-            # desc=f'Type either {self.python_script_mode} for python scripts or {self.cmd_mode} for command line '
 
         self.program_content_arg: ToolArg = self.create_arg(
             name='program_content', dtype=str,
             desc='The content of the Python script or shell command to execute')
 
-    @staticmethod
-    def reader_thread(pipe, q):
+        self.logging_backlog = ''
+        self.shell_session = self.get_shell_session()
+        self.is_error_state = False
+        self.last_msg_time = time.time()
+
+
+        threading.Thread(target=self.log_when_idle).start()
+        threading.Thread(target=self.read_std_out).start()
+        threading.Thread(target=self.read_std_err).start()
+
+
+    def log_when_idle(self) -> None:
         while True:
-            line = pipe.readline()
-            if line:
-                q.put(line)
+            current_time = time.time()
+            if self.is_error_state:
+                logger = self.exception_log
+                flag_str = 'Standard Error'
             else:
-                break
+                logger = self.update_log
+                flag_str = 'Standard Output'
+
+            if current_time - self.last_msg_time >= 0.25 and self.logging_backlog != '':
+                logger(f'{flag_str}\n{self.logging_backlog}')
+                self.logging_backlog = ''
+                self.is_error_state = False
+                self.last_msg_time = time.time()
+
+            time.sleep(0.05)
+
+    def read_std_out(self) -> None:
+        for line in iter(self.shell_session.stdout.readline, ''):
+            cleaned_line = line.strip()
+            if cleaned_line != '':
+                self.logging_backlog += f'{cleaned_line}\n'
+
+
+    def read_std_err(self) -> None:
+        for line in iter(self.shell_session.stderr.readline, ''):
+            cleaned_line = line.strip()
+            if cleaned_line != '':
+                self.is_error_state = True
+                self.logging_backlog += f'{cleaned_line}\n'
+
+
+    @staticmethod
+    def get_shell_session() -> Optional[Popen]:
+        shell_cmd = 'cmd.exe' if RUN.os_in_use == 'Windows' else '/bin/sh'
+        try:
+            shell_session = subprocess.Popen(shell_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        except Exception as e:
+            print(f'[Error]: An exception occured while trying to start terminal session using {shell_cmd}: {e}')
+            shell_session = None
+
+        return shell_session
+
+    # --------------------------------------------
+    #
 
     def do(self) -> None:
         mode = self.mode_arg.val
@@ -74,23 +95,19 @@ class RUN(Tool):
             return
 
         if mode == self.python_script_mode:
-            execution_with_return_result = self.execute_py
+            execute = self.execute_py
         else:
-            execution_with_return_result = self.execute_cmd
+            execute = self.execute_cmd
 
         try:
-            result = execution_with_return_result()
-
-            if result.stdout:
-                self.update_log(f'Standard Output:\n{result.stdout}')
-            if result.stderr:
-                self.exception_log(f'Standard Error:\n{result.stderr}')
+            execute()
+            time.sleep(0.5)
 
         except Exception as e:
             self.exception_log(f'An exception occured during program execution: {e}')
 
 
-    def execute_py(self):
+    def execute_py(self) -> None:
         with tempfile.NamedTemporaryFile(mode='w+', delete=False, suffix='.py') as temp:
             temp.write(self.program_content_arg.val)
             temp_file_path = temp.name
@@ -98,31 +115,17 @@ class RUN(Tool):
         result = subprocess.run(['python', temp_file_path], text=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         os.unlink(temp_file_path)
 
-        return result
+        if result.stdout:
+            self.update_log(f'Standard Output:\n{result.stdout}')
+        if result.stderr:
+            self.exception_log(f'Standard Error:\n{result.stderr}')
 
 
-
-    def execute_cmd(self):
+    def execute_cmd(self)  -> None:
         if self.shell_session is None:
             self.update_log(f'No shell executable set. Aborting RUN ...')
             return
 
         self.shell_session.stdin.write(self.program_content_arg.val + '\n')
         self.shell_session.stdin.flush()
-
-        time.sleep(0.1)  # Adjust the time as needed
-
-        stdout_data = []
-        stderr_data = []
-
-        # Collect stdout
-        while not self.stdout_q.empty():
-            stdout_data.append(self.stdout_q.get().strip())
-
-        # Collect stderr
-        while not self.stderr_q.empty():
-            stderr_data.append(self.stderr_q.get().strip())
-
-        return CommandResult(stdout='\n'.join(stdout_data), stderr='\n'.join(stderr_data))
-
 
