775adc4b72bdc4b98f58c67af2ba4cbc1a279dc6
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-09-26 13:50:10 +0200

    Fixed a misspelling in READ and RUN which caused missing descriptions

--- src/l1_lotus_tools/m0_toolbox/file_io.py
+++ src/l1_lotus_tools/m0_toolbox/file_io.py
@@ -12,7 +12,7 @@ class READ(Tool):
 
     def __init__(self):
         super().__init__()
-        self.description = 'The READ tool allows you to read the contents of a plain text file or a pdf.'
+        self.desc = 'The READ tool allows you to read the contents of a plain text file or a pdf.'
 
         self.fpath_arg : ToolArg = self.create_arg(
             name='fpath', dtype=str,
--- src/l1_lotus_tools/m0_toolbox/run.py
+++ src/l1_lotus_tools/m0_toolbox/run.py
@@ -20,8 +20,8 @@ class RUN(Tool):
 
     def __init__(self):
         super().__init__()
-        self.description = ('The RUN tool allows you to either run a Python script or execute a command line command as input string.'
-                            '')
+        self.desc = f'The RUN tool allows you to either run a Python script or execute a command line command as input string.'
+
         self.mode_arg: ToolArg = self.create_arg(
             name='mode', dtype=str,
             available_options=[RUN.python_script_mode,RUN.cmd_mode],
