b9dac5d40a70523ce5c580baa250b31d9a2fd71a
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-10-03 01:13:50 +0200

    Renamed run directory

--- src/l0_lotus_run/main.py
+++ src/l0_lotus_run/main.py
@@ -1,7 +1,7 @@
 import time
 from typing import Optional
-from src.l0_run.entities import DefaultAgent, User
-from src.l0_run.gui_element import ChatGUI
+from src.l0_lotus_run.entities import DefaultAgent, User
+from src.l0_lotus_run.gui_element import ChatGUI
 from src.l2_lotus_core import Channel, ConversationParticipant,SettingsController
 
 # ---------------------------------------------------------
