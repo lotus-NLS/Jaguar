47d200988afe3e08b6fc5f35be1b2173591b73f1
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-10-05 18:30:50 +0200

    Rearranged code in RUN

--- src/l1_lotus_tools/m0_toolbox/run.py
+++ src/l1_lotus_tools/m0_toolbox/run.py
@@ -36,56 +36,10 @@ class RUN(Tool):
         self.is_error_state = False
         self.last_msg_time = time.time()
 
-        threading.Thread(target=self.log_when_idle).start()
-        threading.Thread(target=self.read_std_out).start()
-        threading.Thread(target=self.read_std_err).start()
+        threading.Thread(target=self.read_terminal_stdout).start()
+        threading.Thread(target=self.read_terminal_stderr).start()
+        threading.Thread(target=self.log_terminal_when_idle).start()
 
-    def log_when_idle(self):
-        while True:
-            current_time = time.time()
-            if self.is_error_state:
-                logger = self.exception_log
-                flag_str = 'Standard Error'
-            else:
-                logger = self.update_log
-                flag_str = 'Standard Output'
-
-            if current_time - self.last_msg_time >= 0.25 and self.logging_backlog != '':
-                logger(f'{flag_str}\n{self.logging_backlog}')
-                self.logging_backlog = ''
-                self.is_error_state = False
-                self.last_msg_time = time.time()
-
-            time.sleep(0.05)
-
-    def read_std_out(self):
-        for line in iter(self.shell_session.stdout.readline, ''):
-            cleaned_line = line.strip()
-            if cleaned_line != '':
-                self.logging_backlog += f'{cleaned_line}\n'
-
-
-    def read_std_err(self):
-        for line in iter(self.shell_session.stderr.readline, ''):
-            cleaned_line = line.strip()
-            if cleaned_line != '':
-                self.is_error_state = True
-                self.logging_backlog += f'{cleaned_line}\n'
-
-
-    @staticmethod
-    def get_shell_session() -> Optional[Popen]:
-        shell_cmd = 'cmd.exe' if RUN.os_in_use == 'Windows' else '/bin/sh'
-        try:
-            shell_session = subprocess.Popen(shell_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        except Exception as e:
-            print(f'[Error]: An exception occured while trying to start terminal session using {shell_cmd}: {e}')
-            shell_session = None
-
-        return shell_session
-
-    # --------------------------------------------
-    #
 
     def do(self):
         mode = self.mode_arg.val
@@ -94,14 +48,8 @@ class RUN(Tool):
             self.semantic_error(f'Invalid mode specified. Use "{self.python_script_mode} for Python script or "{self.cmd_mode}" for command line.')
             return
 
-        if mode == self.python_script_mode:
-            execute = self.execute_py
-        else:
-            execute = self.execute_cmd
-
         try:
-            execute()
-            time.sleep(0.5)
+            self.execute_py() if mode == self.python_script_mode else self.execute_terminal()
 
         except Exception as e:
             self.exception_log(f'An exception occured during program execution: {e}')
@@ -121,7 +69,7 @@ class RUN(Tool):
             self.exception_log(f'Standard Error:\n{result.stderr}')
 
 
-    def execute_cmd(self) :
+    def execute_terminal(self) :
         if self.shell_session is None:
             self.update_log(f'No shell executable set. Aborting RUN ...')
             return
@@ -129,3 +77,49 @@ class RUN(Tool):
         self.shell_session.stdin.write(self.program_content_arg.val + '\n')
         self.shell_session.stdin.flush()
 
+    # --------------------------------------------
+
+    @staticmethod
+    def get_shell_session() -> Optional[Popen]:
+        shell_cmd = 'powershell' if RUN.os_in_use == 'Windows' else '/bin/sh'
+        try:
+            shell_session = subprocess.Popen(shell_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        except Exception as e:
+            print(f'[Error]: An exception occured while trying to start terminal session using {shell_cmd}: {e}')
+            shell_session = None
+
+        return shell_session
+
+
+    def log_terminal_when_idle(self):
+        while True:
+            current_time = time.time()
+            if self.is_error_state:
+                logger = self.exception_log
+                flag_str = 'Standard Error'
+            else:
+                logger = self.update_log
+                flag_str = 'Standard Output'
+
+            if current_time - self.last_msg_time >= 0.25 and self.logging_backlog != '':
+                logger(f'{flag_str}\n{self.logging_backlog}')
+                self.logging_backlog = ''
+                self.is_error_state = False
+                self.last_msg_time = time.time()
+
+            time.sleep(0.05)
+
+
+    def read_terminal_stdout(self):
+        for line in iter(self.shell_session.stdout.readline, ''):
+            cleaned_line = line.strip()
+            if cleaned_line != '':
+                self.logging_backlog += f'{cleaned_line}\n'
+
+
+    def read_terminal_stderr(self):
+        for line in iter(self.shell_session.stderr.readline, ''):
+            cleaned_line = line.strip()
+            if cleaned_line != '':
+                self.is_error_state = True
+                self.logging_backlog += f'{cleaned_line}\n'
