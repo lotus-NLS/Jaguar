d595c949974860b19809cff691ede40b04c5c553
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-10-05 03:41:06 +0200

    Included scratch for LIFO user input retrieval

--- src/l3_lotus_core/m1_OperatorIO/userIO.py
+++ src/l3_lotus_core/m1_OperatorIO/userIO.py
@@ -1,3 +1,47 @@
+# import threading
+# import time
+# from queue import Queue
+#
+# class BlockingObject:
+#     def __init__(self):
+#         self.q = Queue()
+#
+#     def write(self, value):
+#         self.q.put(value)
+#
+#     def read(self):
+#         return self.q.get()
+#
+# class UserIO:
+#     def __init__(self):
+#         self.blockingObjects : list[BlockingObject] = []
+#
+#     def loop(self):
+#         while True:
+#             user_input = input('Write something')
+#             self.blockingObjects[-1].write(user_input)
+#             del self.blockingObjects[-1]
+#
+#     def echo_output(self,the_id: str):
+#         the_thing = self.get_input()
+#         print(f'Process {the_id} got: {the_thing}')
+#
+#     def get_input(self):
+#         input_retriever = BlockingObject()
+#         self.blockingObjects.append(input_retriever)
+#         return input_retriever.read()
+#
+#
+# def start_in_thread(funct):
+#     threading.Thread(target=funct).start()
+#
+#
+# user_io = UserIO()
+# start_in_thread(user_io.loop)
+# start_in_thread(lambda: user_io.echo_output(the_id='1'))
+# start_in_thread(lambda: user_io.echo_output(the_id='two'))
+
+
 class UserIO:
 
     @staticmethod
