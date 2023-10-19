c192ac0fffaf1f9da7ead5409810f452866bbf06
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-10-19 12:55:00 +0200

    Fixed imports; Moved entries test to testing directory

--- src/l2_lotus_agent/__init__.py
+++ src/l2_lotus_agent/__init__.py
@@ -1,3 +1,4 @@
-from .m0_agent.agent import Agent, Task
+from .m0_agent import Agent, Task
+from .m1_models import Tokenizer
 from .m2_protocol import Objective
 from .m2_tool_interface import ToolInterface,ToolArg
--- src/l2_lotus_agent/m1_models/__init__.py
+++ src/l2_lotus_agent/m1_models/__init__.py
@@ -1,3 +1,4 @@
 from .llm import LLM
 from .openAI_model import OpenAI_ModelTypes
 from .openAI_model import OpenAIModel
+from .tokenizer import Tokenizer
