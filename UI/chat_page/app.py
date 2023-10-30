db53b0b2c40501eeb9e45cd0f11c7282044064b8
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-10-30 21:37:12 +0100

    UI: Updated DocumentWriter with new methods

--- UI/chat_page/app.py
+++ UI/chat_page/app.py
@@ -1,19 +1,8 @@
 from flask import Flask
 from yattag import Doc
 
-class DocumentWriter:
-    def __init__(self):
-        self.doc, self.tag, self.text = Doc().tagtext()
+# ----------------------------------------------
 
-    def add_script(self, script_type, content):
-        with self.tag('script', type=script_type):
-            self.doc.asis(content)
-
-    def get_value(self):
-        return self.doc.getvalue()
-
-
-app = Flask(__name__)
 
 brython_script_content = """
 from browser import document
@@ -30,27 +19,61 @@ document["Send"].bind("click", append_message)
 """
 
 
+class DocumentWriter:
+    def __init__(self):
+        self.doc, self.tag, self.text = Doc().tagtext()
+
+    def add_script(self, script_type, content):
+        with self.add_tag('script', type=script_type):
+            self.add_plain_text(content)
+
+    def add_plain_text(self, text):
+        self.doc.asis(text)
+
+    def add_tag(self, tag_name, *args, **kwargs):
+        return self.tag(tag_name, *args, **kwargs)
+
+    def add_stag(self, tag_name, **attributes):
+        self.doc.stag(tag_name, **attributes)
+
+    def get_value(self) -> str:
+        return self.doc.getvalue()
+
+    # ----------------------------------------------
+
+    def get_index(self) -> str:
+        with self.add_tag('html', lang='en'):
+            self.get_head()
+            self.get_body()
+        return self.get_value()
+
+
+    def get_head(self):
+        with self.add_tag('head'):
+            self.add_plain_text('<meta charset="UTF-8">')
+            with self.add_tag('title'):
+                self.text('Flask & Brython Chat')
+            self.add_plain_text('<script src="https://cdn.jsdelivr.net/npm/brython@3.11/brython.min.js"></script>')
+
+    def get_body(self):
+        with self.add_tag('body', onload="brython()"):
+            with self.add_tag('div', id='chat-window', style='height:300px; border:1px solid #ccc; overflow:auto;'):
+                self.text('<!-- Chat history will go here -->')
+            self.add_stag('input', type='text', id='text_bar')
+            self.add_stag('input', type='button', id='Send', value='Send')
+            self.add_script('text/python', brython_script_content)
+
+# ----------------------------------------------
+
+
+app = Flask(__name__)
+
 @app.route('/')
-def index():
+def route_index():
     writer = DocumentWriter()
-
-    with writer.tag('html', lang='en'):
-        with writer.tag('head'):
-            writer.doc.asis('<meta charset="UTF-8">')
-            with writer.tag('title'):
-                writer.text('Flask & Brython Chat')
-            writer.doc.asis('<script src="https://cdn.jsdelivr.net/npm/brython@3.11/brython.min.js"></script>')
-
-        with writer.tag('body', onload="brython()"):
-            with writer.tag('div', id='chat-window', style='height:300px; border:1px solid #ccc; overflow:auto;'):
-                writer.text('<!-- Chat history will go here -->')
-            writer.doc.stag('input', type='text', id='text_bar')
-            writer.doc.stag('input', type='button', id='Send', value='Send')
-
-            writer.add_script('text/python', brython_script_content)
-
-    return writer.get_value()
-
+    return writer.get_index()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
