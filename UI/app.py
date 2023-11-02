d424d63afc398ada0d08ec99a7dc699415a231dc
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-11-02 18:27:50 +0100

    UI: Introduced Input types

--- UI/app.py
+++ UI/app.py
@@ -1,16 +1,21 @@
-from pywebdev.devkit import PyWebApp
+from pywebdev.devkit import PyWebApp, InputType, Style
 
 # ----------------------------------------------
 
 class ChatHTML(PyWebApp):
     def add_body_content(self):
+        # Chat window
+        # chat_window_style = Style(height='300px', border='1px solid #ccc', overflow='auto')
+        # self.add_tag(tag_name='div', id='chat-window', style=chat_window_style)
         with self.add_tag('div', id='chat-window', style='height:300px; border:1px solid #ccc; overflow:auto;'):
-            self.text('<!-- Chat history will go here -->')
+            self.text('')
 
-        self.add_input(input_type='text', the_id='text_bar')
-        self.add_input(input_type='button', the_id='Send', value='Send')
-        self.add_python_script(rel_path='scripts/simple_script.py')
+        # Text and send button
+        self.add_input(input_type=InputType.text(), the_id='text_bar')
+        self.add_input(input_type=InputType.button(), the_id='Send', value='Send')
 
+        # Chat Interactivity
+        self.add_python_script(relPath='scripts/send_routine.py')
 
 
 if __name__ == '__main__':
