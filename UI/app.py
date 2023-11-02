9d1b53001fe06681a1883ecc86e42147e7b46808
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-11-02 21:13:11 +0100

    UI: Adjusted body for pywebdev changes

--- UI/app.py
+++ UI/app.py
@@ -8,11 +8,11 @@ class ChatHTML(PyWebApp):
         # chat_window_style = Style(height='300px', border='1px solid #ccc', overflow='auto')
         # self.add_tag(tag_name='div', id='chat-window', style=chat_window_style)
         with self.add_tag('div', id='chat-window', style='height:300px; border:1px solid #ccc; overflow:auto;'):
-            self.text('')
+            self.add_text('')
 
         # Text and send button
-        self.add_input(input_type=InputType.text(), the_id='text_bar')
-        self.add_input(input_type=InputType.button(), the_id='Send', value='Send')
+        self.add_text_field(the_id='text_bar')
+        self.add_button(the_id='Send', value ='Send')
 
         # Chat Interactivity
         self.add_python_script(relPath='scripts/send_routine.py')
