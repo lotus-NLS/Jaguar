7f73cb01027ff682535e8796635915295dacdc06
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-11-06 11:10:56 +0100

    run: Updated LotusServer for client libs

--- run.py
+++ run.py
@@ -14,11 +14,14 @@ class LotusServer:
 
         @self.web_app.route('/api/<path:filename>')
         def api_lib(filename):
-            return send_from_directory('/api', filename)
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            api_dir = os.path.join(current_dir, 'api')
+            return send_from_directory(api_dir, filename)
 
         @self.web_app.route('/client/<path:filename>')
         def client_lib(filename):
-            clientdir = os.path.join('UI', 'scripts/client')
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            clientdir = os.path.join(current_dir,'UI', 'scripts/client')
             return send_from_directory(clientdir, filename)
 
     def run(self):
@@ -34,6 +37,7 @@ class LotusServer:
         def do_web_app_run():
             self.web_app.run()
         DaemonThread(target=do_web_app_run()).start()
+
 
 the_server = LotusServer()
 the_server.run()
