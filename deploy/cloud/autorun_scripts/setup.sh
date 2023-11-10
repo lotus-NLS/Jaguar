72c461c02a498d2b424abe9b1dbc4a63a627e7f8
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-11-10 19:14:52 +0100

    deploy: Updated setup script

--- deploy/cloud/autorun_scripts/setup.sh
+++ deploy/cloud/autorun_scripts/setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-cd ~/Downloads
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
+sudo apt-get update --fix-missing
 
-cd ~
+
 git clone https://github.com/Somerandomguy10111/Lotus
 git clone https://github.com/Somerandomguy10111/pystuff
 export PYTHONPATH="$(pwd)/Lotus"
 export PYTHONPATH="$(pwd)/pystuff"
 cd Lotus
+
+sudo apt install python3.10-venv
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
