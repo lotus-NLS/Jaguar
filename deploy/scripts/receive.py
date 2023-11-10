89ca41ea2f69ba20596b621a988633fd7508d118
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-11-10 17:04:06 +0100

    deploy: Fixed agent message display by introducing AgentChat class

--- deploy/scripts/receive.py
+++ deploy/scripts/receive.py
@@ -4,33 +4,33 @@ from lib import ParagraphConverter
 
 # ----------------------------------------------
 
+class AgentChat:
+    def __init__(self):
+        self.last_element = None
+        self.paragraph_converter = ParagraphConverter()
+
+
+    def make_new_entry(self,entry : Entry):
+        self.paragraph_converter.add_markdown(markdown_text=f"{entry.get_name()}: {entry.get_content()}")
+        new_element = document.createElement("div")
+        new_element.innerHTML = self.paragraph_converter.get()
+        self.last_element = new_element
+        chat_window.appendChild(new_element)
+
+    def handle_engine_data(self, msg : dict):
+        entry_content : str = msg['data']
+        new_entry : Entry = Entry.from_serialized_str(s=entry_content)
+        print(f'Agent entry arrived with content: {new_entry.get_content().__repr__()}; Flags: {new_entry.flags.as_text()}')
+
+        if new_entry.flags.get(Flag.IS_ENTRY_START) or self.last_element is None:
+            self.paragraph_converter.reset()
+            self.make_new_entry(entry=new_entry)
+        else:
+            self.paragraph_converter.add_markdown(markdown_text=new_entry.get_content())
+            self.last_element.innerHTML = self.paragraph_converter.get()
+
+
 chat_window = document["chat-window"]
-
-
-def make_new_entry(entry : Entry):
-    paragraph_converter.add_markdown(markdown_text=f"{entry.get_name()}: {entry.get_content()}")
-    new_element = document.createElement("div")
-    new_element.innerHTML = paragraph_converter.get()
-
-
-def handle_engine_data(msg : dict):
-    entry_content : str = msg['data']
-    new_entry : Entry = Entry.from_serialized_str(s=entry_content)
-    print(f'Agent entry arrived with content: {new_entry.get_content().__repr__()}; Flags: {new_entry.flags.as_text()}')
-
-    try:
-        last_element = chat_window.children[-1]
-    except:
-        last_element = None
-
-    if new_entry.flags.get(Flag.IS_ENTRY_START) or last_element is None:
-        paragraph_converter.reset()
-        make_new_entry(entry=new_entry)
-    else:
-        paragraph_converter.add_markdown(markdown_text=new_entry.get_content())
-        last_element.innerHTML = paragraph_converter.get()
-
-
-paragraph_converter = ParagraphConverter()
+agent_chat = AgentChat()
 evt_source = window.EventSource.new(f'/{Ends.engine_data.identifier}')
-evt_source.onmessage = handle_engine_data
+evt_source.onmessage = agent_chat.handle_engine_data
