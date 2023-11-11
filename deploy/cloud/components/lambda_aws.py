f6ead30a6209f80664defb5268b0257d8ac9e300
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-11-11 14:51:12 +0100

    cloud/lambdas: Included get_zip function for lambda generation

--- deploy/cloud/components/lambda_aws.py
+++ deploy/cloud/components/lambda_aws.py
@@ -2,6 +2,8 @@ import boto3
 import inspect
 import io
 import zipfile
+import os, tempfile, shutil, sys
+import subprocess
 
 from .enums import AWSRegions
 # ----------------------------------------------
@@ -40,25 +42,78 @@ class LambdaManager:
         Deploys a lambda for an entirely self contained function the_function
         Necessary imports must be stated within the function body not in the header of the module
         """
+
+            # Path to site-packages in venv
+            # site_packages = os.path.join(venv_path, 'lib', f'python{sys.version_info.major}.{sys.version_info.minor}',
+            #                              'site-packages')
+            #
+            # # Copy site-packages contents to the temporary directory
+            # shutil.copytree(site_packages, os.path.join(tmp_dir, 'site-packages'), dirs_exist_ok=True)
+            # shutil.copy2(python_file_path, tmp_dir)
+            #
+            # # Create a zip file from the temporary directory
+            # zip_path = shutil.make_archive('lambda_package', 'zip', tmp_dir)
+
+
+
         try:
+            funct_name = the_function.__name__
+            zip_content = self.get_zip(the_function=the_function)
 
-            zip_buffer = io.BytesIO()
-            with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
-                zip_file.writestr('pyfunct.py', inspect.getsource(the_function))
-
-            fname = the_function.__name__
             response = self.lambda_client.create_function(
-                FunctionName=fname,
+                FunctionName=funct_name,
                 Runtime='python3.10',
                 Role=role_arn,
-                Handler=f'pyfunct.{fname}',
-                Code={'ZipFile': zip_buffer.getvalue()},
+                Handler=f'pyfunct.{funct_name}',
+                Code={'ZipFile': zip_content},
             )
-            print(f"Lambda function deployed: {fname}")
+
+            print(f"Lambda function deployed: {funct_name}")
             return response['FunctionArn']
+
 
         except Exception as e:
             print(f"An error occurred: {e}")
+
+    @staticmethod
+    def get_zip(self, the_function : callable):
+        funct_name = the_function.__name__
+        python_src = inspect.getsource(the_function)
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+
+            # make src folder and python script
+            srcdir_name = 'srcdir'
+            os.makedirs(srcdir_name,exist_ok=True)
+            python_file_name = f"{funct_name}.py"
+            python_file_path = os.path.join(tmp_dir,srcdir_name, python_file_name)
+            with open(python_file_path, 'w') as python_file:
+                python_file.write(python_src)
+
+
+            # setup venv
+            subprocess.run([sys.executable, "-m", "venv", "venv"], check=True)
+            venv_python = os.path.join("venv", "bin", "python")
+            subprocess.run([venv_python, "-m", "pip", "install", "pipreqs"], check=True)
+
+            # Install generate requirements and install
+            subprocess.run(["pipreqs", srcdir_name], check=True)
+            command = f"source venv/bin/activate && pip install -r {srcdir_name}/requirements.txt"
+            subprocess.run(command, shell=True, executable='/bin/bash', check=True)
+
+            # package venv
+            site_packages = os.path.join('venv', 'lib',
+                                         f'python{sys.version_info.major}.{sys.version_info.minor}',
+                                         'site-packages')
+
+            # Copy site-packages contents to the temporary directory
+            shutil.copytree(site_packages, os.path.join(tmp_dir, 'site-packages'), dirs_exist_ok=True)
+
+            zip_path = shutil.make_archive('lambda_package', 'zip', tmp_dir)
+            with open(zip_path, 'rb') as zip_file:
+                zip_content = zip_file.read()
+
+        return zip_content
 
 
     # def schedule_lambda_backup(self, function_arn: str, schedule_expression: str):
