7db25a0eac043661c06407e0e1dbc3c6e94fb0f5
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-11-13 18:30:37 +0100

    run: Reverted ip and port

--- run.py
+++ run.py
@@ -37,7 +37,7 @@ class LotusServer:
 
     def launch_web_app(self):
         def do_web_app_run():
-            self.web_app.run(host='0.0.0.0', port=80)
+            self.web_app.run(host='127.0.0.1', port=5000)
         DaemonThread(target=do_web_app_run()).start()
 
 
