69cedfb80cdf040a13db3a3c7bbc398707358b5d
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-05 01:01:43 +0100

    tests: Fixed lotus settings tests

--- tests/l5/t_settings.py
+++ tests/l5/t_settings.py
@@ -13,6 +13,9 @@ class TestLotusSettings(Unittest):
         cls.valid_keys = ['openai_api_key', 'google_api_key', 'search_engine_id', 'enable_introduction']
         cls.invalid_keys = ['invalid_key', 'invalid_key2', 'invalid_key3']
 
+    def tearDown(self):
+        self.settings.reset_instance()
+
     # --------------------------------------------
     # tests
 
