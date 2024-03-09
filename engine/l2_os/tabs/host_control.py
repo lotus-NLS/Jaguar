816d5530d83e7c9fdf94dc78e0a37404e741dca5
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-09 15:22:10 +0100

    l2: Fixed syntax typos in HostWorkspace

--- engine/l2_os/tabs/host_control.py
+++ engine/l2_os/tabs/host_control.py
@@ -12,16 +12,16 @@ class HostWorkspace(Workspace):
         self.input_grid = Grid(20,20)
         self.text_mouse = TextMouse(input_grid=self.input_grid)
         self.keyboard = Keyboard()
+        self.display = Display.get_primary()
 
     def get_text(self) -> str:
         return ''
 
     def get_image(self) -> Optional[PILImage]:
-        display = Display.get_primary()
-        display.get_screenshot(grid=self.input_grid)
+        return self.display.get_screenshot(grid=self.input_grid)
 
-    def click(self, x : int, y : int):
-        self.text_mouse.click(x, y)
+    def click(self, cell_num : int):
+        self.text_mouse.click(cell_num=cell_num, on_primary_display=self.display.is_primary)
 
     def type(self, msg : str):
         self.keyboard.type(msg=msg)
