7949385c7ce810af98bafac3f5bb9d98fd1a3520
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-24 00:32:28 +0100

    l2: LotusText now exclusively uses TextFile

--- engine/l2_os/workspaces/text.py
+++ engine/l2_os/workspaces/text.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import os.path
 from typing import Optional
 from PIL.Image import Image as PILImage
 
@@ -13,24 +12,21 @@ class LotusText(Workspace):
         super().__init__()
         self.content : Optional[str] = None
         self.text_file : Optional[TextFile] = None
-        self.fpath : Optional[str] = None
 
 
     def open(self, filepath : str):
         self.text_file = TextFile(fpath=filepath, require_writable=True)
-        self.fpath = filepath
 
     def close(self, *args, **kwargs):
         pass
 
     def get_text(self) -> str:
-        if not self.fpath:
-            return ''
-        if not os.path.isfile(self.fpath):
-            return ''
+        try:
+            text = self.text_file.read()
+            lines = text.splitlines()
+        except:
+            lines = ['']
 
-        with open(self.fpath, 'r') as f:
-            lines = f.readlines()
         numbered_lines = [f"{i + 1} | {line}" for i, line in enumerate(lines)]
         msg = ''.join(numbered_lines)
         return msg
@@ -41,30 +37,25 @@ class LotusText(Workspace):
     # ---------------------------------------------------------
 
     def insert(self, line: int, content: str):
-        if self.text_file.get_suffix() == 'pdf':
-            raise ValueError('Cannot edit pdf files')
         if line <= 0:
             raise ValueError("Line number must be a positive integer.")
-
         try:
-            with open(self.fpath, 'r') as f:
-                lines = f.readlines()
+            text = self.text_file.read()
+            lines = text.splitlines()
         except:
             lines = []
         index = line - 1
         lines.insert(index, content)
-        with open(self.fpath, 'w') as f:
-            f.writelines(lines)
+
+        text = '\n'.join(lines)
+        self.text_file.write(content=text)
 
 
     def delete_lines(self, start_line: int, end_line: int):
-        if self.text_file.get_suffix().lower() == 'pdf':
-            raise ValueError('Cannot edit pdf files')
         if start_line <= 0 or end_line < start_line:
             raise ValueError("Invalid line range.")
 
-        with open(self.fpath, 'r') as f:
-            lines = f.readlines()
+        lines = self.text_file.read()
         del lines[start_line - 1:end_line]
-        with open(self.fpath, 'w') as f:
-            f.writelines(lines)
+        newcontent = '\n'.join(lines)
+        self.text_file.write(newcontent)
