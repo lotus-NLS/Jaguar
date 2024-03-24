fac45e118c75e6224f9a547b330ac7d30a9551c2
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-24 10:01:46 +0100

    tests: Updated tests for workspace updates

--- tests/l2/test_workspace.py
+++ tests/l2/test_workspace.py
@@ -17,19 +17,19 @@ class TestWorkspaceOpenClose(WorkspaceTest):
         self.assertFalse(self.workspace.is_active)
 
     def test_open_application(self):
-        self.workspace.on_open()
+        self.workspace.open_action.do()
         self.assertTrue(self.workspace.is_active)
         self.assertIsInstance(self.workspace, MockWorkspace)
 
     def test_close_application(self):
-        self.workspace.on_open()
-        self.workspace.on_close()
+        self.workspace.open_action.do()
+        self.workspace.close_action.do()
         self.assertFalse(self.workspace.is_active)
 
 
 class TestWorkspaceActions(WorkspaceTest):
     def test_action_generation(self):
-        self.workspace.on_open()
+        self.workspace.open_action.do()
         action_names = [action.get_name() for action in self.workspace.get_actions()]
         print(f'Action names are: {action_names}')
         for text in ['add', 'reset']:
@@ -47,10 +47,11 @@ class TestWorkspaceActions(WorkspaceTest):
             self.assertIsInstance(action, Action)
 
     def test_num_actions(self):
+        self.workspace.open_action.do()
         actions = self.workspace.get_actions()
         actions_info = [action.get_name() for action in actions]
         self.log(f'Actions are : {actions_info}')
-        self.assertEqual(2, len(actions))
+        self.assertEqual(3, len(actions))
 
     def test_invalid_args(self):
         with self.assertRaises(TypeError):
@@ -59,25 +60,36 @@ class TestWorkspaceActions(WorkspaceTest):
 
 class TestActionExecution(WorkspaceTest):
     def test_tool_execution(self):
-        self.workspace.open_action(uri='test_path')
-        add = self.workspace.tool_dict['add']
-        reset = self.workspace.tool_dict['reset']
+        self.workspace.open_action.do()
+        actions = self.workspace.get_actions()
+        actions_map : dict[str, Action] = {action.get_name() : action for action in actions}
+
+        add, reset = None, None
+        for key, value in actions_map.items():
+            print(key)
+            if 'add' in key:
+                add = value
+            if 'reset' in key:
+                reset = value
+        if add is None or reset is None:
+            raise ValueError('Add or reset action not found')
+
 
         add_json_str = '{"msg": "New text"}'
         tool_call = ToolCall(json_str=add_json_str)
 
         add.handle(tool_call=tool_call)
-        self.assertIn('New text', self.workspace.window.workspace[0].get_text())
-        self.log(f'Window context before reset: {self.workspace.window.get_entry()}')
+        self.assertIn('New text', self.workspace.get_text())
+        self.log(f'Window context before reset: {self.workspace.get_entry()}')
 
         reset_json_str = '{}'
         tool_call = ToolCall(json_str=reset_json_str)
         reset.handle(tool_call)
-        self.assertEqual('', self.workspace.window.workspace[0].get_text())
-        self.log(f'Window context after reset : {self.workspace.window.get_entry()}')
+        self.assertEqual('', self.workspace.get_text())
+        self.log(f'Window context after reset : {self.workspace.get_entry()}')
 
 
 if __name__ == '__main__':
-    TestWorkspaceOpenClose.execute_all()
-    TestWorkspaceActions.execute_all()
-    # TestActionProperties.execute_all()
+    # TestWorkspaceOpenClose.execute_all()
+    # TestWorkspaceActions.execute_all()
+    TestActionExecution.execute_all()
