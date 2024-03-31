bbf075b8425d1403806404c8a151f48d80989c3c
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-01 01:54:27 +0200

    l2: Introduced playwright scratch for browser interaction

--- engine/l2_os/workspaces/browser.py
+++ engine/l2_os/workspaces/browser.py
@@ -43,3 +43,85 @@
 #         content = site_visitor.get_text(site_url=self.url_arg.val)
 #         self.log(f'Content found at {self.url_arg.val}:\n{content}', phase=Update.UPDATE)
 #
+
+
+
+# from playwright.async_api import async_playwright
+#
+#
+# async def list_all_links_texts_and_focusable_elements():
+#     async with async_playwright() as p:
+#         browser = await p.chromium.launch()
+#         page = await browser.new_page()
+#         await page.goto('https://example.com')
+#
+#         # Getting all links
+#         links = await page.evaluate('''() => {
+#             return Array.from(document.querySelectorAll('a')).map(link => link.href);
+#         }''')
+#
+#         focusable_elements_selector = 'input:not([disabled]), textarea:not([disabled]), a[href]:not([tabindex="-1"]), select:not([disabled]), button:not([disabled]), [tabindex]:not([tabindex="-1"])'
+#         focusable_elements = await page.locator(focusable_elements_selector).element_handles()
+#
+#         # Extracting details of focusable elements
+#         focusable_details = []
+#         for element in focusable_elements:
+#             tag = await element.evaluate('(element) => element.tagName.toLowerCase()')
+#             type_attr = await element.get_attribute('type')
+#             value_or_placeholder = await element.input_value() if tag in ['input',
+#                                                                           'textarea'] else await element.get_attribute(
+#                 'placeholder')
+#             focusable_details.append({'tag': tag, 'type': type_attr, 'value_or_placeholder': value_or_placeholder})
+#
+#         print("Focusable Elements:", focusable_details)
+#
+#         # Getting all text on the page
+#         all_text = await page.evaluate('''() => {
+#             return document.body.innerText;
+#         }''')
+#
+#         print("Links:", links)
+#         print("Focusable Fields:", focusable_fields)
+#         print("All Text on Page:", all_text)
+# 
+#         await browser.close()
+#
+#
+# # Execute the modified function
+# import asyncio
+#
+# asyncio.run(list_all_links_texts_and_focusable_elements())
+#
+# from playwright.async_api import async_playwright
+#
+#
+# async def list_focusable_elements():
+#     async with async_playwright() as p:
+#         browser = await p.chromium.launch()
+#         page = await browser.new_page()
+#         await page.goto('https://example.com')
+#
+#         # Locating focusable elements by checking if they can be focused by keyboard
+#         # This includes elements that are inputs, buttons, links with hrefs, or have a tabindex that's not negative.
+#         focusable_elements_selector = 'input:not([disabled]), textarea:not([disabled]), a[href]:not([tabindex="-1"]), select:not([disabled]), button:not([disabled]), [tabindex]:not([tabindex="-1"])'
+#         focusable_elements = await page.locator(focusable_elements_selector).element_handles()
+#
+#         # Extracting details of focusable elements
+#         focusable_details = []
+#         for element in focusable_elements:
+#             tag = await element.evaluate('(element) => element.tagName.toLowerCase()')
+#             type_attr = await element.get_attribute('type')
+#             value_or_placeholder = await element.input_value() if tag in ['input',
+#                                                                           'textarea'] else await element.get_attribute(
+#                 'placeholder')
+#             focusable_details.append({'tag': tag, 'type': type_attr, 'value_or_placeholder': value_or_placeholder})
+#
+#         print("Focusable Elements:", focusable_details)
+#
+#         await browser.close()
+#
+#
+# # Execute the function
+# import asyncio
+#
+# asyncio.run(list_focusable_elements())
