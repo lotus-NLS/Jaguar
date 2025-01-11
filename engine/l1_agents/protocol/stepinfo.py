761c3a1c1e6ec398188a2dbedef74244c2add085
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-01-11 03:18:04 +0100

    l1: Added labels, recursive completion to Objective

--- engine/l1_agents/protocol/stepinfo.py
+++ engine/l1_agents/protocol/stepinfo.py
@@ -23,32 +23,44 @@ class StepInfo:
         call_options = CallOptions(call_allowed=True, required_tool_name=self.required_tool)
         return Options(call_options=call_options)
 
-class Task:
-    def __init__(self, content : str):
-        self.subtasks : list[Task] = []
+class Objective:
+    def __init__(self, content : str, identifier : str = 'A', is_root : bool = False):
+        self.is_root : bool = is_root
         self.content : str = content
+        self.identifier : str = identifier
+
         self.is_complete : bool = False
+        self.subtasks : list[Objective] = []
 
     def complete(self):
         self.is_complete = True
+        for st in self.subtasks:
+            st.complete()
 
     def add_subtask(self, msg : str):
-        new_task = Task(content=msg)
+        new_task = Objective(content=msg, identifier=f'{self.identifier}{len(self.subtasks)+1}')
         self.subtasks.append(new_task)
         return new_task
 
     def get_tree(self, pre_indent : str = '') -> str:
-        conditional_mark = 'x' if self.is_complete else ''
-        tree = f'{pre_indent}[{conditional_mark}] {self.content}\n'
+        if not self.is_root:
+            conditional_mark = 'x' if self.is_complete else ' '
+            tree = f'{pre_indent}[{conditional_mark}] {self.identifier}: {self.content}\n'
+        else:
+            tree = ''
 
         for st in self.subtasks:
-            indent = '\t' + pre_indent
+            indent = '\t' + pre_indent if not self.is_root else ''
             tree += f'{st.get_tree(pre_indent=indent)}'
 
         return tree
 
 
 class Workflowy(Workspace):
+    def __init__(self):
+        super().__init__()
+        self.root_objective : Objective = Objective()
+
     def close(self, *args, **kwargs):
         pass
 
@@ -61,9 +73,6 @@ class Workflowy(Workspace):
     def get_image(self) -> Optional[PILImage]:
         return None
 
-    def __init__(self):
-        super().__init__()
-        self.root_objective : Task = Task()
 
     def open(self, yaml_str : str):
         pass
