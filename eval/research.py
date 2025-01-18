44953efdf6a01573a302ce0790c4ae284573f4e8
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-01-18 01:37:46 +0100

    eval: Added reasoning step to assertProperty

--- eval/research.py
+++ eval/research.py
@@ -1,12 +1,10 @@
 from api import Entry
 from engine import LotusEngine
-from engine.l1_agents import Workflowy
-from holytools.devtools import Unittest
-
 from engine.l0_engine.settings import LotusCredentials
+from engine.l2_models import OpenAIModel, Context, Options
 from engine.l2_models.llm import LLM
 from engine.l3_aos.tools import Tool, ToolArg
-from engine.l2_models import OpenAIModel, Context, Options
+from holytools.devtools import Unittest
 
 
 # ---------------------------------------------------
@@ -32,32 +30,43 @@ class SemanticUnittest(Unittest):
         configs = LotusCredentials()
         cls.model : LLM = OpenAIModel.default_model(api_key=configs.get_openai_apikey())
 
-    def assertProperty(self, msg : str, property_query : str):
+    def assertProperty(self, msg : str, prop : str):
         yn = YesNoTool()
-        entries = [Entry.agent(msg=msg), Entry.user(msg=property_query)]
+        prop = (f'Please evaluate whether or not the following #property holds for the given #msg'
+                          f'    - #property: {prop}'
+                          f'    - #msg {msg}')
+        entries = [Entry.user(msg=prop)]
         docs = [yn.get_doc()]
-        contet = Context(entries=entries, docs=docs)
+        context = Context(entries=entries, docs=docs)
 
-        generation = self.model.get_generation(context=contet, options=Options.require_call(tool_name=f'YesNoTool'))
+        generation = self.model.get_generation(context=context, options=Options.text_only())
         generation.exhaust()
         text, calls = generation.get_text(), generation.get_tool_calls()
+        print(f'-> Generated text: {text}')
+
+        context += Context.singleton(entry=Entry.agent(msg=text))
+        options = Options.require_call(tool_name=yn.get_name())
+        yn_generation = self.model.get_generation(context=context, options=options)
+        yn_generation.exhaust()
+        text, calls = yn_generation.get_text(), yn_generation.get_tool_calls()
 
         self.assertTrue(len(calls) == 1)
         yn.execute(tool_call=calls[0])
 
-        print(f'\"{property_query}\": {yn.y_n_arg.get_value()}')
+        print(f'\"{prop}\": {yn.y_n_arg.get_value()}')
         self.assertTrue(yn.y_n_arg.get_value() == 'y')
 
 
 class HardwareTask(SemanticUnittest):
     def test_components_there(self):
         engine = LotusEngine()
-        answer = engine.resarch_routine(workflowy=Workflowy._hardware_summary(),
-                                        query=f'Please give me a summary of my hardware', max_steps=5)
+        #answer = engine.resarch_routine(workflowy=Workflowy._hardware_summary(),
+        #                                query=f'Please give me a summary of my hardware', max_steps=5)
 
-        property_query = (f'The given answer provdes information about the following hardware devices:'
+        property_query = (f'The ##Eval statement gives information about each of the following hardware devices:'
                           f'CPU, GPU, RAM, Disks and Motherboard')
-        self.assertProperty(msg=answer, property_query=property_query)
+        answer = 'GOTO: '
+        self.assertProperty(msg=answer, prop=property_query)
 
 
 if __name__ == "__main__":
