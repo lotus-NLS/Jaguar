237f6366bf09354d63e0cf52951c1a8d2392ebeb
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-02-13 12:15:44 +0100

    l3: Rebuild browser workspace through BrowserEmulator

--- engine/l3_aos/workspaces/browser.py
+++ engine/l3_aos/workspaces/browser.py
@@ -4,6 +4,7 @@ from PIL.Image import Image as PILImage
 
 from engine.l3_aos.workspace import Workspace
 from pyscrape.search import SearchEngine
+from pyscrape.browse import BrowserEmulator
 
 
 # ---------------------------------------------------------
@@ -13,119 +14,32 @@ class Browser(Workspace):
     def __init__(self, google_api_key : str, searchengine_id : str):
         super().__init__()
         self.search_engine : SearchEngine = SearchEngine(google_api_key=google_api_key, searchengine_id=searchengine_id)
+        self.emulator : BrowserEmulator = BrowserEmulator()
+        self.search_context : str = ''
 
     def open(self, url: str):
-        pass
+        """Starts a text based browser and opens the given URL. Use URL=search://{search_term} to perform a google search insted"""
+        self.visit(url=url)
 
     def close(self):
         pass
 
-    def get_text(self):
-        pass
+    def visit(self, url : str):
+        """Opens the specified url. Use url=search://{search_term} to perform a google search insted"""
+        if url.startswith('search://'):
+            search_tearm = url.replace(f'search://','')
+            results = self.search_engine.get_results(search_term=search_tearm, num_results=5)
+            search_results = f'Results for search term \"{search_tearm}\"\n'
+            for index, scrape in enumerate(results):
+                search_results += f'({index}): {scrape}\n'
+            self.search_context = search_results
+        self.emulator.visit(url=url)
 
-    def get_desc(self) -> str:
-        pass
+    def get_text(self):
+        return self.emulator.get_markdown()
 
     def get_image(self) -> Optional[PILImage]:
-        pass
+        return None
 
-
-
-# class Browser(Workspace):
-#     def __init__(self, google_api_key : str, searchengine_id : str):
-#         super().__init__()
-#         self.current_dir_path: Optional[str] = None
-#         self.show_hidden : bool = True
-#         self.currrent_url : Optional[str] = None
-#         self.site_visitor : Optional[SiteVisitor] = None
-#         self.search_engine : SearchEngine = SearchEngine(google_key=google_api_key, searchengine_id=searchengine_id)
-#         self.search_context : Optional[str] = None
-#
-#     def open(self, url: str):
-#         """Opens the specified scrape. Use url=search://{search_term} to perform a google search insted"""
-#         if url.startswith('search://'):
-#             search_tearm = url.replace(f'search://','')
-#             self.google_search(search_term=search_tearm)
-#         else:
-#             self.visit_site(url=url)
-#
-#
-#     def close(self):
-#         """Close Browser"""
-#         self.currrent_url = None
-#         if self.site_visitor:
-#             self.site_visitor.quit()
-#         self.site_visitor = None
-#
-#     def google_search(self, search_term : str, num_results : int = 4):
-#         """Googles the search term and displays links/summaries of top results"""
-#         results = self.search_engine.get_results(search_term=search_term, num_results=num_results)
-#         self.search_context = f'Results for search term \"{search_term}\"\n'
-#         for index, scrape in enumerate(results):
-#             self.search_context += f'({index}): {scrape}\n'
-#
-#     def visit_site(self, url : str):
-#         """Visits the selected scrape in the browser"""
-#         self.currrent_url = url
-#         self.site_visitor = SiteVisitor(headless=False)
-#
-#     # ---------------------------------------------------------
-#     # context
-#
-#     def get_image(self) -> Optional[PILImage]:
-#         return None
-#
-#
-#     def get_text(self) -> str:
-#         browser_text = ''
-#         if self.search_context:
-#             browser_text += f'{self.search_context}\n'
-#         if self.currrent_url:
-#             if self.site_visitor.site_exists(url=self.currrent_url):
-#                 browser_text += self._get_site_text()
-#             else:
-#                 browser_text += f'---> Site does not exist: {self.currrent_url}\n'
-#         return browser_text
-#
-#     def _get_site_text(self) -> str:
-#         info_text = f'---> Currently visiting scrape: {self.currrent_url}\n'
-#         page_source = self.site_visitor.get_html(url=self.currrent_url)
-#         soup = BeautifulSoup(page_source, 'html.parser')
-#         links = soup.find_all('a')
-#
-#         def get_repr(link):
-#             link_text = link.get_text(strip=True)
-#             content = link.get('href')
-#
-#             parsed_url = urlparse(self.currrent_url)
-#             base_domain = f"{parsed_url.scheme}://{parsed_url.netloc}"
-#
-#             if not content.startswith('http'):
-#                 prefix = '' if content.startswith('/') else '/'
-#                 content = f'{base_domain}{prefix}{content}'
-#
-#             return f"[{link_text}]({content})"
-#
-#         def link_qualifes(link):
-#             href_content = link.get('href')
-#             if not href_content:
-#                 return False
-#             return True
-#
-#         markdown_links = [get_repr(link) for link in links if link_qualifes(link)]
-#         links_content = '\n'.join(markdown_links)
-#         site_text = f'- Site text: \n{self.site_visitor.get_text(url=self.currrent_url)}\n'
-#         links_text = f'- Links: \n {links_content}'
-#
-#
-#         return f'{info_text}{site_text}\n{links_text}'
-#
-#
-#     def get_desc(self) -> str:
-#         return f"A browser allowing you to perform a google search and scrape sites"
-
-
-
-
-
-
+    def get_desc(self) -> str:
+        return f"A browser allowing you to search google and browse sites"
