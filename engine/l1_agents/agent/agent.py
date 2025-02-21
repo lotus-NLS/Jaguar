3f7c550522de4cb52b234cc3cb7bddabdf941ba2
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-02-21 21:44:42 +0100

    l1/agent: Fixed get_tools logic

--- engine/l1_agents/agent/agent.py
+++ engine/l1_agents/agent/agent.py
@@ -90,7 +90,7 @@ class Agent(Loggable):
 
     def get_context(self) -> Context:
         context = Context(entries=[self.identity.as_system_entry()])
-        context += Context.from_workspaces(workspaces=self.get_active_workspaces())
+        context += Context.from_workspaces(workspaces=self.get_workspaces(active_only=True))
         context += Context(entries=self.memory)
 
         if self.workflowy.is_active:
@@ -106,18 +106,21 @@ class Agent(Loggable):
     # tools
 
     def get_tools(self) -> list[Tool]:
-        workspaces = self.get_active_workspaces()
+        workspaces = self.get_workspaces()
         tools = []
         for ws in workspaces:
             tools += ws.get_actions()
         return tools
 
     def get_ws(self, name : str) -> Workspace:
-        ws_map = {ws.get_name(): ws for ws in self.get_active_workspaces()}
+        ws_map = {ws.get_name(): ws for ws in self.get_workspaces()}
         return ws_map[name]
 
-    def get_active_workspaces(self) -> list[Workspace]:
-        workspaces = [ws for ws in  self.aos.workspaces if ws.is_active]
-        if self.workflowy.is_active:
-            workspaces += [self.workflowy]
+    def get_workspaces(self, active_only : bool = False) -> list[Workspace]:
+        workspaces = []
+        workspaces += self.aos.workspaces
+        workspaces += [self.workflowy]
+        if active_only:
+            workspaces = [ws for ws in workspaces if ws.is_active]
+
         return workspaces
