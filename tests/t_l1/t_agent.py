a8cf42ad62deb359ae1fca0399de489236dabe0d
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-02-28 08:59:54 +0100

    tests/l1: Added checks in test_required_tool

--- tests/t_l1/t_agent.py
+++ tests/t_l1/t_agent.py
@@ -1,6 +1,7 @@
 from engine.l1_agents import Agent
 from engine.l2_models import OpenAIModel, InfConfig
 from engine.l3_aos import Browser, Terminal, AOS
+from engine.l3_aos.tools import ToolOutput
 from tests.credtest import CredTest
 from tests.t_l2.base import Greet
 
@@ -18,7 +19,14 @@ class TestAgent(CredTest):
     def test_required_tool(self):
         greet_tool = Greet()
         inf_config = InfConfig(required_tool=greet_tool)
-        self.agent.handle(inf_config=inf_config)
+        step = self.agent.handle(inf_config=inf_config)
+
+        outputs : list[ToolOutput] = step.outputs
+
+        self.assertTrue(len(outputs) == 1)
+        self.assertTrue(outputs[0].tool_name == greet_tool.get_name())
+
+
 
 if __name__ == "__main__":
     TestAgent.execute_all()
