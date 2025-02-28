a593bb2a5cfc0a69b4c9be7310e1937728dc97bd
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-02-28 09:24:25 +0100

    l1: Added exit decision tool

--- engine/l1_agents/guidance/workflow.py
+++ engine/l1_agents/guidance/workflow.py
@@ -1,23 +1,57 @@
+from __future__ import annotations
 from dataclasses import dataclass
 
 from engine.l1_agents import Task
+from engine.l3_aos.tools import Tool, ToolArg
 
+# ------------------------------------------------
 
 @dataclass
 class Node:
     content : str
     max_steps : int
     task : Task
+    exit_cases : list[ExitCase]
+
+    def get_exit_tool(self) -> ExitTool:
+        return ExitTool(exit_cases=self.exit_cases)
 
 @dataclass
-class Edge:
-    content : str
-    source : Node
-    target : Node
+class ExitCase:
+    description : str
+    target_node : Node
+
 
 @dataclass
 class Workflow:
     start_node: Node
     nodes : list[Node]
-    edges : list[Edge]
 
+    # @classmethod
+    # def get_example_workflow(cls) -> Workflow:
+    #     start_node = Node(content='Start', max_steps=3, task=Task.get_example_task())
+    #     nodes = [start_node]
+    #     return cls(start_node=start_node, nodes=nodes)
+
+
+class ExitTool(Tool):
+    def __init__(self, exit_cases : list[ExitCase]):
+        super().__init__()
+        self.exit_cases : list[ExitCase] = exit_cases
+        self.exit_choice : ToolArg = ToolArg(name='Exit choice', dtype=int)
+
+    def do(self):
+        pass
+
+    def get_desc(self) -> str:
+        initial_msg = f'Decides with which case the current task is quit:'
+        for j, c in enumerate(self.exit_cases):
+            initial_msg += f'\n[{j}]: {c.description}\n'
+        return initial_msg
+
+    def get_args(self) -> list[ToolArg]:
+        return [self.exit_choice]
+
+
+if __name__ == "__main__":
+    pass
