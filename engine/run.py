caca291d8d0d4aa2b4defe1a3f8ea44461214f80
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-04-30 15:54:18 +0200

    engine: Reintroduced conversation routine in run

--- engine/run.py
+++ engine/run.py
@@ -2,4 +2,10 @@ from engine.l0_main.lotus_engine import LotusEngine
 
 if __name__ == "__main__":
     engine = LotusEngine()
-    engine.do_talk(query='Hi there!')
+    while True:
+        user_input = input("User:")
+        if user_input == 'exit':
+            break
+        engine.do_talk(query=user_input)
+        print()
+
