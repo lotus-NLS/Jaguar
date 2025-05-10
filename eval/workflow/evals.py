de17fb7a1a5387f3939f8eca92553f6867439140
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-05-10 21:06:51 +0200

    evals: Elimianted workflow source imports;

--- eval/workflow/evals.py
+++ eval/workflow/evals.py
@@ -1,12 +1,12 @@
-from wfeval import WorkflowEval
-from frame.build import TestNVDATicker
+from eval.workflow.frame import TestNVDATicker
+from eval.workflow.wfeval import WorkflowEval
+
 
 class BuildEval(WorkflowEval):
     def test_build_nvda_ticker(self):
         success = self.evaluate_unittest(unittest=TestNVDATicker)
         if not success:
             self.fail('Failed to pass unittest')
-
 
 if __name__ == '__main__':
     BuildEval.execute_all()
